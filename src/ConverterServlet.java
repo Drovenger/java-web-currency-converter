9757bf7d420bcb33c5d1414b643ccd07d9d5b573
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-07-30 17:10:21 +0700

    [Thực hành] Ứng dụng chuyển đổi tiền tệ - Servlet

---
+++ src/ConverterServlet.java
@@ -0,0 +1,44 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ConverterServlet", urlPatterns = "/convert")
+public class ConverterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float rate = Float.parseFloat(request.getParameter("rate"));
+        float usd = Float.parseFloat(request.getParameter("usd"));
+
+        float vnd = rate * usd;
+
+        try (PrintWriter printWriter = response.getWriter()) {
+            printWriter.println("<html>");
+            printWriter.println("<h1>Rate: " + rate + "</h1>");
+            printWriter.println("<h1>USD: " + usd + " USD</h1>");
+            printWriter.println("<h1>VND: " + vnd + " VND</h1>");
+            printWriter.println("</html>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float rate = Float.parseFloat(request.getParameter("rate"));
+        float usd = Float.parseFloat(request.getParameter("usd"));
+
+        float vnd = rate * usd;
+
+        try (PrintWriter printWriter = response.getWriter()) {
+            printWriter.println("<html>");
+            printWriter.println("<h1>Rate: " + rate + "</h1>");
+            printWriter.println("<h1>USD: " + usd + " USD</h1>");
+            printWriter.println("<h1>VND: " + vnd + " VND</h1>");
+            printWriter.println("</html>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
